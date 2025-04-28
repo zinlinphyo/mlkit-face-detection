87b4a511bbe67912148d9591cd7dba38aa908dce
Zin Lin Phyo <zinlinphyo.dev@gmail.com>
2025-04-28 10:15:51 +0700

    Initial Commit

---
+++ mlkit-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mlkit-test
+//
+//  Created by Zin Lin Phyo on 28/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
